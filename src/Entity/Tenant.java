055f02781c0aa9311f25f44168e1fcb57e70f2ff
amifiroos <65414133+amifiroos@users.noreply.github.com>
2020-06-16 22:04:27 +0700

    Update Tenant.java

--- src/Entity/Tenant.java
+++ src/Entity/Tenant.java
@@ -1,28 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Entity;
 
 import java.util.ArrayList;
-
-/**
- *
- * @author Angiospermae
- */
 public class Tenant {
     int idtenant;
     String namatenant;
-    
 
-    
     public Tenant(int idtenant, String namatenant){
         this.idtenant = idtenant;
         this.namatenant = namatenant;
         
     }
-    
-    
 }

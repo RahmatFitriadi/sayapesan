bf5c39c2d526df26770a13b8747e9be211ddd328
amifiroos <65414133+amifiroos@users.noreply.github.com>
2020-06-16 22:57:54 +0700

    Update config.java

--- src/Koneksi/config.java
+++ src/Koneksi/config.java
@@ -1,22 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Koneksi;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
-/**
- *
- * @author Angiospermae
- */
 public class config {
     public Connection con;
     public Statement stmt;
-    
     public void config(){
         try{
             Class.forName("com.mysql.jdbc.Driver");

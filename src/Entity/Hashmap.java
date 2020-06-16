f2c7c6431549ab96571d8ee13c4ba311ac2a3211
amifiroos <65414133+amifiroos@users.noreply.github.com>
2020-06-16 21:50:09 +0700

    Update Hashmap.java

--- src/Entity/Hashmap.java
+++ src/Entity/Hashmap.java
@@ -1,16 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Entity;
 import java.util.HashMap;
 import java.util.Map;
-/**
- *
- * @author Angiospermae
- */
 public class Hashmap {
     public static void main(String[] args) {
     
@@ -19,8 +9,6 @@ public class Hashmap {
         hashMap.put(11, "Faturrahman");
         hashMap.put(12, "Dedi");
         hashMap.put(13, "Munandar");
-        
-        //System.out.println("Nama : "+hashMap.get(11));
         
         for (Map.Entry map : hashMap.entrySet()){
             System.out.println(map.getKey()+" = "+map.getValue());
